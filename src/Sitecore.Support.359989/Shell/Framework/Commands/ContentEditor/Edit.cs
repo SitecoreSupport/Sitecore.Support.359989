f2225001c2d92474233449f2a867a2e222074371
sc-ArtemKosohov <ark@sitecore.net>
2019-10-22 14:06:40 +0300

    patched 'contenteditor:edit' command added

---
+++ src/Sitecore.Support.359989/Shell/Framework/Commands/ContentEditor/Edit.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Support.Shell.Framework.Commands.ContentEditor
+{
+    using Sitecore;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Shell.Framework.Commands;
+    
+    public class Edit : Sitecore.Shell.Framework.Commands.ContentEditor.Edit
+    {
+        public override CommandState QueryState(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+
+            if (context.Items.Length != 1)
+            {
+                return CommandState.Hidden;
+            }
+            Item item = context.Items[0];
+            if (item.IsFallback)
+            {
+                return CommandState.Disabled;
+            }
+            return ((!base.HasField(item, FieldIDs.Workflow) || !base.HasField(item, FieldIDs.WorkflowState)) ? CommandState.Hidden : (HasWriteAccess(item) ? (item.Access.CanWriteLanguage() ? (!CanCheckIn(item) ? (!CanEdit(item) ? CommandState.Disabled : CommandState.Enabled) : CommandState.Down) : CommandState.Disabled) : CommandState.Disabled));
+        }
+
+
+    }
+}
